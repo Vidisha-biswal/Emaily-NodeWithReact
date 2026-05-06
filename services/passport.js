e60eb8d0b0e0e540e562082842ee1972be7d0618
Vidisha Biswal <biswalvidisha31@gmail.com>
2026-05-06 10:45:09 +0530

    React Folder

--- services/passport.js
+++ services/passport.js
@@ -22,22 +22,19 @@ passport.use(
         clientSecret:keys.googleClientSecret,
         callbackURL:'/auth/google/callback',
         proxy: true 
-    }, (accessToken,refreshToken,profile,done)=>{
+    }, 
+    async (accessToken,refreshToken,profile,done)=>{
 
-        User.findOne({googleId:profile.id})
-            .then((existingUser)=>{
-                if(existingUser)
-                {
-                    done(null,existingUser);
-                    //1st arg-> error obj which is null for now
-                    //2nd arg-> user record
-                }
-                else{
-                    //mongoose model instance
-                    new User({ googleId:profile.id})
-                        .save()
-                        .then(user=> done(null,user));
-                }
-            })
+        const existingUser=await User.findOne({googleId:profile.id});
+        if(existingUser)
+        {
+            return done(null,existingUser);
+            //1st arg-> error obj which is null for now
+            //2nd arg-> user record
+        }
+        //mongoose model instance
+        const user=await new User({ googleId:profile.id}).save();
+        done(null,user);
+        
     })
 );
