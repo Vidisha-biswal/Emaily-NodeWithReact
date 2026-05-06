f24733045483f4f268f0b768cd496ae9956d9c48
Vidisha Biswal <biswalvidisha31@gmail.com>
2026-05-06 15:57:00 +0530

    fetching user

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import { legacy_createStore  as createStore,applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
