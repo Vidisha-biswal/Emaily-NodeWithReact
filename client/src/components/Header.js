cc4793a8b597096002a380bcabc787adef095e79
Vidisha Biswal <biswalvidisha31@gmail.com>
2026-05-07 22:20:42 +0530

    added stripe

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, {Component, component} from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom'
 
 class Header extends Component{
     renderContent(){
@@ -22,9 +23,11 @@ class Header extends Component{
         return (
             <nav>
                 <div className='nav-wrapper'>
-                    <a className='left brand-logo' href='localhost:3000/surveys'>
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/'} 
+                        className='left brand-logo' >
                         Emaily
-                    </a>
+                    </Link>
                     <ul className='right'>
                         {this.renderContent()}
                     </ul> 
