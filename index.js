fb21bb703e1a932af534d62fa6239a50615b9e03
Vidisha <biswalvidisha31@gmail.com>
2026-05-17 18:12:28 +0530

    added billing routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cookieSession=require('cookie-session');
 const passport=require('passport');
+const bodyParser=require('body-parser');
 const keys=require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -20,9 +21,11 @@ mongoose.connect(keys.mongoURI, {
 
 
 const authRoutes=require('./routes/authRoutes');
+const billingRoutes=require('./routes/billingRoutes');
 
 const app=express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge:30*24*60*60*1000,
@@ -33,6 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
+billingRoutes(app);
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, () => {
