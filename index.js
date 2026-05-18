d3a47a361e967a462ef9072aef72efece6142b4f
Vidisha <biswalvidisha31@gmail.com>
2026-05-18 22:25:02 +0530

    path changed

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(passport.session());
 
 authRoutes(app);
 billingRoutes(app);
-
+/*
 if(process.env.NODE_ENV==='production')
 {
     app.use(express.static('client/build'));
@@ -46,7 +46,7 @@ if(process.env.NODE_ENV==='production')
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
 }
-    /*
+    */
 
 // ✅ NEW FIXED CODE
 if (process.env.NODE_ENV === 'production') {
@@ -56,12 +56,11 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     // Express will serve the index.html file if it doesn't recognize the route
-    app.get('*', (req, res) => {
+    app.get('(.*)', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-*/
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
