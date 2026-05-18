d2fd926a194a5e767d158a76b966f1239e96acd7
Vidisha <biswalvidisha31@gmail.com>
2026-05-18 22:39:30 +0530

    apply universal regex routing fix

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     // Express will serve the index.html file if it doesn't recognize the route
-    app.get('/:path*', (req, res) => {
+    // app.get('/:path*', (req, res) => {
+    app.get(/^(?!\/api).*$/, (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
