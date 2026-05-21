30f1c3a7cd272aeea2174a0c30db4a86b4cab9fb
Vidisha <biswalvidisha31@gmail.com>
2026-05-21 16:35:33 +0530

    components added and changes

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,21 @@
+//form for user input
+import React, {Component} from 'react';
+import { reduxForm ,Field } from 'redux-form';
+
+class SurveyForm extends Component{
+    render(){
+        return (
+            <div>
+                <Field
+                    type="text"
+                    name="surveyTitle"
+                    component="input"
+                />
+            </div>
+        );
+    };
+}
+ 
+export default reduxForm({
+    form: 'surveyForm',
+})(SurveyForm);
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,11 @@
+components/surveys/SurveyNew.js//it shows SurveyForm ans SurveyFormReviewimport React, {Component} from 'react';import SurveyForm from './SurveyForm';
+class SurveyNew extends Component{
+    render(){
+         return (
+     <div>
+     <SurveyForm />
+ </div>
+ );
+ };
+}
+ export default SurveyNew;
