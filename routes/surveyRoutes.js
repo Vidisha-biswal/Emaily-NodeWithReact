dd7bf4f4e05cf35bf032022f9c0cd914ea3854a3
Vidisha <biswalvidisha31@gmail.com>
2026-05-21 16:27:52 +0530

    client file changed

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ module.exports= app=>{
     app.get('/api/surveys/thanks',(req,res)=>{
         res.send('Thanks for voting!');
     });
-    app.post('/api/surveys',requireLogin,requireCredits,(req,res)=>{    
+    app.post('/api/surveys',requireLogin,requireCredits, async (req,res)=>{    
         const {title,subject,body, recipients}= req.body;    
         const survey= new Survey({      
             title,      
