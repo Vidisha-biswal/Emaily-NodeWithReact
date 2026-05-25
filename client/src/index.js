3075d6b742a3347ea91df9b61a2a06f64746a7d3
Vidisha <biswalvidisha31@gmail.com>
2026-05-25 19:53:23 +0530

    changes done

--- client/src/index.js
+++ client/src/index.js
@@ -15,5 +15,3 @@ const store=createStore(reducers, {}, applyMiddleware(reduxThunk));
 const el=document.getElementById("root");
 const root=ReactDOM.createRoot(el);
 root.render(<Provider store={store}><App /></Provider>);
-console.log("STRIPE KEY", process.env.REACT_APP_STRIPE_KEY);
-console.log("Environment", process.env.NODE_ENV);
